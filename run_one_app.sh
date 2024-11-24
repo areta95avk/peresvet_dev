a0fe78bbfbc2714e3172d21419a44c49afc5f356
Vovaman <bd-postbox1@mail.ru>
2024-11-24 14:23:07 +0300

    #128 in progress

--- run_one_app.sh
+++ run_one_app.sh
@@ -3,14 +3,14 @@
 # в первом аргументе можно задать имя сервера, в качестве которого по умолчанию
 # принимается имя текущего сервера
 
-# -f docker/compose/docker-compose.ports.yml \
-
 
 srv=$HOSTNAME
 if [ -n "$1" ]
 then
     srv=$1
 fi
+
+# 
 
 sed -i "s/NGINX_HOST=.*/NGINX_HOST=$srv/" docker/compose/.cont_one_app.env
 docker compose --env-file docker/compose/.cont_one_app.env \
@@ -21,4 +21,5 @@ docker compose --env-file docker/compose/.cont_one_app.env \
 -f docker/compose/docker-compose.one_app.yml \
 -f docker/compose/docker-compose.grafana.yml \
 -f docker/compose/docker-compose.nginx.one_app.yml \
+-f docker/compose/docker-compose.ports.yml \
 up
